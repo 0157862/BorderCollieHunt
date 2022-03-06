a8de7ea3965f44f21c042458fa71d8b477b9d708
Jessica <0157862@schoolsnet.act.edu.au>
2022-03-07 10:29:25 +1100

    start

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,16 +1,8 @@
 extends Control
 
+export(int) var countdownMax
+var currentTimer
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	set_process(true)
-
-func _process(delta):
-	pass
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
+	currentTimer = countdownMax
+	$HUD/Countdown.text = str(currentTimer)
--- Player/Player.gd
+++ Player/Player.gd
@@ -13,12 +13,10 @@ func _ready():
 	set_process(true)
 	set_physics_process(true)
 
-func _physics_process(delta):
-	pass
-			
-func _process(delta):
-	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_up"):
+		move_and_collide(Vector2(-movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_down"):
+		move_and_collide(Vector2(movement_speed * delta, 0))
