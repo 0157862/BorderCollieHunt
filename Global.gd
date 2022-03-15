1bc0e5c7b2f0a6b4f8d8d9c28bff06df063f1d6c
Jessica <0157862@schoolsnet.act.edu.au>
2022-03-15 15:28:16 +1100

    Stuff

--- Global.gd
+++ Global.gd
@@ -3,6 +3,8 @@ extends Node
 var bulletInstanceCount = 0 # Keeps track of how many bullet instances are current
 var enemyBulletInstanceCount = 0
 
+var previousScore = []
+
 var scoringInformation = {
 	"currentScore": 0,
 	"currentPlayer": "User",
